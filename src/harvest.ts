59f5979678dcdfda4e899ecbf66bc75a30a633f3
Serge K <cmt@phnx47.net>
2021-10-09 23:23:03 +0700

    feat: get current week data

--- src/harvest.ts
+++ src/harvest.ts
@@ -10,14 +10,14 @@ export default class Harvest {
   debug: boolean;
   private accessToken: string;
   private accountId: string;
-  private axiosClient: AxiosInstance;
+  private axiosInstance: AxiosInstance;
 
   constructor(config: Config) {
     this.debug = config.debug || false;
     this.accessToken = config.accessToken;
     this.accountId = config.accountId;
 
-    this.axiosClient = axios.create({
+    this.axiosInstance = axios.create({
       baseURL: 'https://api.harvestapp.com/api/v2',
       headers: {
         'User-Agent': 'x-report',
@@ -26,5 +26,24 @@ export default class Harvest {
         'Content-Type': 'application/json'
       }
     });
+  }
+
+  getCurrentWeek() {
+    var curr = new Date;
+    var first = curr.getDate() - curr.getDay();
+
+    var firstDay = new Date(curr.setDate(curr.getDate() - curr.getDay()));
+    var lastDay = new Date(curr.setDate(first + 6))
+
+    var from = firstDay.toISOString().slice(0, 10).replaceAll('-', '');
+    var to = lastDay.toISOString().slice(0, 10).replaceAll('-', '')
+
+    this.axiosInstance.get(`/time_entries?from=${from}&to=${to}`)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      })
   }
 }
