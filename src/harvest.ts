e4acc6bfd85190b0f4433a3242505f2b217e9862
Serge K <cmt@phnx47.net>
2021-10-09 22:43:49 +0700

    feat: replace fetch by axios

--- src/harvest.ts
+++ src/harvest.ts
@@ -1,3 +1,5 @@
+import axios, { AxiosInstance } from 'axios';
+
 export interface Config {
   debug?: boolean;
   accessToken: string;
@@ -5,14 +7,24 @@ export interface Config {
 }
 
 export default class Harvest {
-  userAgent: string = 'x-report';
   debug: boolean;
   private accessToken: string;
   private accountId: string;
+  private axiosClient: AxiosInstance;
 
   constructor(config: Config) {
     this.debug = config.debug || false;
     this.accessToken = config.accessToken;
     this.accountId = config.accountId;
+
+    this.axiosClient = axios.create({
+      baseURL: 'https://api.harvestapp.com/api/v2',
+      headers: {
+        'User-Agent': 'x-report',
+        'Authorization': `Bearer ${this.accessToken}`,
+        'Harvest-Account-Id': this.accountId,
+        'Content-Type': 'application/json'
+      }
+    });
   }
 }
