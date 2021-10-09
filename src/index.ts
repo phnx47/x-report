a099a342deb26cf8883627480ab2dfb7272dd635
Serge K <cmt@phnx47.net>
2021-10-09 23:23:03 +0700

    feat: get current week data

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,12 @@ if (!process.env.HARVEST_ACCESS_TOKEN)
   throw new Error('Environment variable "HARVEST_ACCESS_TOKEN" is not defined')
 
 if (!process.env.HARVEST_ACCOUNT_ID)
-  throw new Error('Environment variable "HARVEST_ACCESS_TOKEN" is not defined')
+  throw new Error('Environment variable "HARVEST_ACCOUNT_ID" is not defined')
 
 const harvest = new Harvest({
   accessToken: process.env.HARVEST_ACCESS_TOKEN,
   accountId: process.env.HARVEST_ACCOUNT_ID
 });
+
+
+harvest.getCurrentWeek();
