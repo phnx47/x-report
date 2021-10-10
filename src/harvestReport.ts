40bfa42cec8d680c43bab5082f0c2efb95823564
Serge K <cmt@phnx47.net>
2021-10-10 20:18:45 +0700

    refactor: extract harvestReport

---
+++ src/harvestReport.ts
@@ -0,0 +1,39 @@
+export interface HarvestReport {
+  time_entries: TimeEntry[];
+  per_page: number;
+  total_pages: number;
+  total_entries: number;
+  next_page?: any;
+  previous_page?: any;
+  page: number;
+}
+
+interface HarvestTask {
+  id: number;
+  name: string;
+}
+
+interface TimeEntry {
+  id: number;
+  spent_date: string;
+  task: HarvestTask;
+  hours: number;
+  hours_without_timer: number;
+  rounded_hours: number;
+  notes: string;
+  created_at: Date;
+  updated_at: Date;
+  is_locked: boolean;
+  locked_reason: string;
+  is_closed: boolean;
+  is_billed: boolean;
+  timer_started_at?: any;
+  started_time: string;
+  ended_time: string;
+  is_running: boolean;
+  external_reference?: any;
+  billable: boolean;
+  budgeted: boolean;
+  billable_rate?: number;
+  cost_rate: number;
+}
