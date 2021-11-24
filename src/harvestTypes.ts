21a67c04a2bd8b3717dd943268a6745483e9ffc6
Serge K <cmt@phnx47.net>
2021-11-24 18:35:58 +0700

    fix: update harvestTypes

--- src/harvestTypes.ts
+++ src/harvestTypes.ts
@@ -3,8 +3,8 @@ export type HarvestReport = {
   per_page: number;
   total_pages: number;
   total_entries: number;
-  next_page?: any;
-  previous_page?: any;
+  next_page?: number;
+  previous_page?: number;
   page: number;
 }
 
@@ -21,19 +21,6 @@ type TimeEntry = {
   hours_without_timer: number;
   rounded_hours: number;
   notes: string;
-  created_at: Date;
-  updated_at: Date;
-  is_locked: boolean;
-  locked_reason: string;
-  is_closed: boolean;
-  is_billed: boolean;
-  timer_started_at?: any;
-  started_time: string;
-  ended_time: string;
-  is_running: boolean;
-  external_reference?: any;
-  billable: boolean;
-  budgeted: boolean;
-  billable_rate?: number;
-  cost_rate: number;
+  created_at: string;
+  updated_at: string;
 }
